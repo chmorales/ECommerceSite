7e755d6529a07f3824a5f9694eafa00e4145386e
Anthony Mulieri <Anthony.Mulieri@stonybrook.edu>
2017-11-01 16:04:47 -0400

    Added awesome quote.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GenericShoppingSite
 A generic marketplace for merchants and shoppers to come together.
+
+"We don't have the server space for foreigners."
--- mysql_scripts/create_tables.sql
+++ mysql_scripts/create_tables.sql
@@ -6,71 +6,74 @@ USE CSE305;
 
 CREATE TABLE address (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    streetNumber INT,
-    streetName VARCHAR(20),
-    city VARCHAR(20),
-    zipCode VARCHAR(5),
-    country VARCHAR(10)
+    streetNumber INT NOT NULL,
+    streetName VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    zipCode VARCHAR(5) NOT NULL,
+    country VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE cart (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    price FLOAT(10,2)
+    price FLOAT(10,2) NOT NULL
 );
 
 CREATE TABLE person (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    password VARCHAR(20),
-    email_address VARCHAR(20) UNIQUE,
-    cartId INT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    email_address VARCHAR(20) UNIQUE NOT NULL,
+    cartId INT NOT NULL,
     FOREIGN KEY (cartId) REFERENCES cart(id)
 );
 
 CREATE TABLE administrator (
-    userId INT,
+    userId INT NOT NULL,
     FOREIGN KEY (userId) REFERENCES person(id)
 );
 
 CREATE TABLE category (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    description VARCHAR(40)
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE item (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    description VARCHAR(100),
-    price FLOAT(10, 2),
-    seller_id INT,
-    quantity INT,
-    category_id INT,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    price FLOAT(10, 2) NOT NULL,
+    seller_id INT NOT NULL,
+    quantity INT NOT NULL,
+    category_id INT NOT NULL,
     FOREIGN KEY (seller_id) REFERENCES person(id),
     FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 CREATE TABLE purchase (
-    buyerId INT,
-    cartId INT,
-    purchaseDate DATETIME,
+    buyerId INT NOT NULL,
+    cartId INT NOT NULL,
+    purchaseDate DATETIME NOT NULL,
     FOREIGN KEY (buyerId) REFERENCES person(id),
     FOREIGN KEY (cartId) REFERENCES cart(id)
 );
 
 CREATE TABLE takenItem (
-    itemId INT,
-    cartId INT,
+    itemId INT NOT NULL,
+    cartId INT NOT NULL,
     FOREIGN KEY (itemId) REFERENCES item(id),
     FOREIGN KEY (cartId) REFERENCES cart(id)
 );
 
 CREATE TABLE review (
-    itemId INT,
-    userId INT,
-    rating INT,
-    description VARCHAR(40),
+    itemId INT NOT NULL,
+    userId INT NOT NULL,
+    rating INT NOT NULL,
+    description VARCHAR(40) NOT NULL,
     FOREIGN KEY (itemId) REFERENCES item(id),
     FOREIGN KEY (userId) REFERENCES person(id)
 );
+
+
+
