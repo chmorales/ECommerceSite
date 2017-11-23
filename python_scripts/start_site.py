511fb58544ae0999ad1cdc0836da52ac27cfbceb
Anthony Mulieri <anthony.mulieri@stonybrook.edu>
2017-11-23 11:59:11 -0500

    Resigned homepage and moved account handling there- allowing for userId to be a session detail.

--- python_scripts/start_site.py
+++ python_scripts/start_site.py
@@ -2,75 +2,94 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = '$ombraM@inBTW'
   
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Flask App!"
-       
-@app.route("/hello/<string:name>/")
-def hello(name):
-    return render_template('test.html',name=name)
-               
-# Account creation page.
-@app.route("/create_account", methods=['GET', 'POST'])
-def create_account():
     if request.method == 'POST':
-        # Going to have to figure something out about this. Everyone's database will have a different root password. 
+        if 'create_account' in request.form:
+            # Going to have to figure something out about this. Everyone's database will have a different root password. 
+            cnx = mysql.connector.connect(user='root', password='tl35tl35', host='localhost', database='CSE305')
+            cursor = cnx.cursor()
+
+            # We need the email address, the password, the firstname, and the lastname. 
+            email_address = request.form['email']
+            password = request.form['password']
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+
+            # TODO: Add checks for validity.
+
+            # Create a new cart just for our user. 
+            query = 'INSERT INTO cart (price) VALUES (%f);' % (0.0)
+            cursor.execute(query)
+
+            # Commit the newly added cart to the database.
+            cnx.commit()
+
+            # The one we just added will be the last one, AKA the max.
+            query = 'SELECT MAX(id) FROM cart'
+            cursor.execute(query)
+            cart_id = int(cursor.fetchall()[0][0])
+
+            # Insert the new user into the database.
+            query = "INSERT INTO person (first_name, last_name, password, email_address, cartId) VALUES (%s, %s, %s, %s, %s)"
+            values = (first_name, last_name, password, email_address, cart_id)
+            cursor.execute(query, values)
+
+            # Commit and close.
+            cnx.commit()
+            cnx.close()
+
+            session['user_id'] = user_id
+            return redirect(url_for('hello'))
+
+        if 'log_in' in request.form:
+            cnx = mysql.connector.connect(user='root', password='tl35tl35', host='localhost', database='CSE305')
+            cursor = cnx.cursor(buffered=True)
+            email_address = request.form['email']
+            password = request.form['password']
+
+            # Doing it this way prevents an injection attack (allegedly).
+            query = ("SELECT u.first_name, u.id FROM person u WHERE u.email_address = %s AND u.password = %s;")
+            data = (email_address, password)
+            cursor.execute(query, data)
+            for (first_name, user_id) in cursor:
+                session['user_id'] = user_id
+                return redirect(url_for('hello'))
+        cnx.close()
+    return render_template('homepage.html')
+       
+@app.route("/hello")
+def hello():
+    return render_template('test.html')
+
+@app.route('/sell_item', methods=['GET', 'POST'])
+def sell_item():
+    if request.method == 'POST':
         cnx = mysql.connector.connect(user='root', password='$ombraM@inBTW', host='104.196.6.60', database='CSE305')
         cursor = cnx.cursor()
 
-        # We need the email address, the password, the firstname, and the lastname. 
-        email_address = request.form['email']
-        password = request.form['password']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
+        name = request.form['name']
+        description = request.form['description']
+        price = float(request.form['price'])
+        quantity = int(request.form['quantity'])
+        category_id = 1
+        # category_name = request.form['category']
 
-        # TODO: Add checks for validity.
+        # query = 'SELECT c.id FROM category c WHERE c.name = %s'
+        # data = (category_name)
+        # cursor.execute(query, data)
+        # category_id = int(cursor.fetchall()[0][0])
 
-        # Create a new cart just for our user. 
-        query = 'INSERT INTO cart (price) VALUES (%f);' % (0.0)
-        cursor.execute(query)
-
-        # Commit the newly added cart to the database.
-        cnx.commit()
-
-        # The one we just added will be the last one, AKA the max.
-        query = 'SELECT MAX(id) FROM cart'
-        cursor.execute(query)
-        cart_id = int(cursor.fetchall()[0][0])
-
-        # Insert the new user into the database.
-        query = "INSERT INTO person (first_name, last_name, password, email_address, cartId) VALUES (%s, %s, %s, %s, %s)"
-        values = (first_name, last_name, password, email_address, cart_id)
-        cursor.execute(query, values)
-
-        # Commit and close.
-        cnx.commit()
-        cnx.close()
-
-    return render_template('create_account2.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        cnx = mysql.connector.connect(user='root', password='$ombraM@inBTW', host='104.196.6.60', database='CSE305')
-        cursor = cnx.cursor(buffered=True)
-        email_address = request.form['email']
-        password = request.form['password']
-
-        # Doing it this way prevents an injection attack (allegedly).
-        query = ("SELECT u.first_name FROM person u WHERE u.email_address = %s AND u.password = %s;")
-        data = (email_address, password)
+        query = 'INSERT INTO item (name, description, price, seller_id, quantity, category_id) VALUES (%s, %s, %s, %s, %s, %s)'
+        data = (name, description, price, session['user_id'], quantity, category_id)
         cursor.execute(query, data)
-        if cursor.rowcount != 0:
-            name = cursor.fetchall()[0][0]
-            return redirect(url_for('hello', name=name))
-        else:
-            print('wrong')
-        cursor.close()
+
+        cnx.commit()
+
         cnx.close()
-    return render_template('log_in.html', error=error)
+    return render_template('sell_item.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
