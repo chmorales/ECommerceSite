62f00733b09df8ddd2711871c316eff1358cf889
Chris <christopher.morales@stonybrook.edu>
2017-11-28 18:49:31 -0500

    Okay i actually fixed it

--- python_scripts/start_site.py
+++ python_scripts/start_site.py
@@ -93,7 +93,7 @@ def index():
             error = 'Invalid email/password combination.'
         
         if 'search_input' in request.form:
-            search_string = request.form['search_input']
+            search_string = request.form['search']
             return redirect(url_for('search_results', string=search_string))
 
         cnx.close()
